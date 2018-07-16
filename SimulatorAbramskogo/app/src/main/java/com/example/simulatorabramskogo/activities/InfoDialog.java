14ea6fd501a9b22e19cfc8e56744e1f91188dad1
Aina <ainaard@mail.ru>
2018-07-16 11:11:02 +0300

    dialog in tasks, frame layout bug fixed

--- SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/InfoDialog.java
+++ SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/InfoDialog.java
@@ -2,11 +2,11 @@ package com.example.simulatorabramskogo.activities;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +19,7 @@ import com.example.simulatorabramskogo.R;
  */
 public class InfoDialog extends DialogFragment {
 
-    MyListener listener;
+//    MyListener listener;
     TextView textView;
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -29,33 +29,32 @@ public class InfoDialog extends DialogFragment {
         AlertDialog.Builder adb = new AlertDialog.Builder(getActivity());
         textView = view.findViewById(R.id.textViewResources);
         adb.setTitle("Подробнее").setMessage("here should be resources").setView(view)
-                .setPositiveButton("ОК", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        listener.mListener();
-                    }
-                });
-        return adb.show();
+                .setPositiveButton("ОК", (dialog, which) -> {
+
+                        }
+//                        listener.mListener()
+                );
+        return adb.create();
 
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof StartActivity) {
-            listener = (MyListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragment1DataListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        listener = null;
-    }
-
-    public void show(FragmentManager fragmentManager, String dialog) {
-    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof StartActivity) {
+//            listener = (MyListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragment1DataListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        listener = null;
+//    }
+//
+//    public void show(FragmentManager fragmentManager, String dialog) {
+//    }
 }
--- SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/fragments/TasksFragment.java
+++ SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/fragments/TasksFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.simulatorabramskogo.MyListener;
 import com.example.simulatorabramskogo.R;
 import com.example.simulatorabramskogo.activities.InfoDialog;
 import com.example.simulatorabramskogo.activities.TaskAdapter;
@@ -27,12 +28,10 @@ import java.util.List;
 public class TasksFragment extends Fragment implements TaskAdapter.TaskDialogInterface{
     List<Action> listOfTasks;
     RecyclerView recyclerView;
-    TaskAdapter.TaskDialogInterface taskDialogInterface;
 
     public TasksFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,7 +42,7 @@ public class TasksFragment extends Fragment implements TaskAdapter.TaskDialogInt
         View view = inflater.inflate(R.layout.fragment_tasks,container,false);
         recyclerView = view.findViewById(R.id.recycleView);
 
-        TaskAdapter taskAdapter = new TaskAdapter(taskDialogInterface, listOfTasks);
+        TaskAdapter taskAdapter = new TaskAdapter(this, listOfTasks);
         recyclerView.setAdapter(taskAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
@@ -52,7 +51,6 @@ public class TasksFragment extends Fragment implements TaskAdapter.TaskDialogInt
 
     @Override
     public void show() {
-        InfoDialog infoDialog = (InfoDialog) InfoDialog.instantiate(getActivity(),"InfoDialog");
-        infoDialog.show(getFragmentManager(), "dialog");
-    }
+        InfoDialog infoDialog = new InfoDialog();
+        infoDialog.show(getFragmentManager(), "info");    }
 }
