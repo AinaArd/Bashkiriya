1a2a1ad9eae4b209fa1d276dfca7211ef2b4c74c
Kamila <kamila-nigmet@mail.ru>
2018-07-18 12:02:29 +0300

    Pictures are different now

--- SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/AchievementDialog.java
+++ SimulatorAbramskogo/app/src/main/java/com/example/simulatorabramskogo/activities/AchievementDialog.java
@@ -6,10 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.simulatorabramskogo.R;
 import com.example.simulatorabramskogo.logic.Achievement;
+import com.example.simulatorabramskogo.logic.AchievementsManager;
 
 /**
  * Created by ${Aina} on 16.07.2018.
@@ -19,6 +21,7 @@ public class AchievementDialog extends DialogFragment {
     Achievement achievement;
     TextView markers;
     TextView congrats;
+    ImageView award;
 
     public android.app.Dialog onCreateDialog(@Nullable Bundle saveInstanceState) {
         super.onCreateDialog(saveInstanceState);
@@ -29,6 +32,11 @@ public class AchievementDialog extends DialogFragment {
         markers.setText(String.valueOf(achievement.getMarkers()));
         congrats = view.findViewById(R.id.congrats);
         congrats.setText(achievement.getName());
+        award = view.findViewById(R.id.imageViewAward);
+        int id = AchievementsManager.getInstance().getCurrentAchievement().getId();
+        String mDrawableName = "ach_"+(id-1);
+        int resID = getResources().getIdentifier(mDrawableName , "drawable", getActivity().getPackageName());
+        award.setImageResource(resID);
         adb.setTitle("Новое достижение!")
                 .setView(view)
                 .setPositiveButton("Ok", (dialog, which) -> dismiss());
